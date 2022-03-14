a0f29608d848e8fa86df81de5f43aebd1375ec9d
sorakann iMac <ski@student.42lausanne.com>
2022-03-14 18:24:25 +0100

    TOUT SEMBLE FONCTIONNER: il faut enlever tout les commentaires et passer la norminette

--- split_to_b.c
+++ split_to_b.c
@@ -15,6 +15,7 @@
 /* ************************************************************************** */
 static int split_to_b_rec(t_data *d, int start, int end);
 static void optimize_rev_rot(t_data *d, int cnt_rot_a, int cnt_rot_b);
+// static void optimize_rot(t_data *d, int *buf_rot_a, int *buf_rot_b);
 
 /* ************************************************************************** */
 // RETURN: group_b_id (from the lowest chunk_b of the chunk_b_group)
@@ -33,6 +34,7 @@ int split_to_b(t_data *d)
 	return (chunk_b_group_id);
 
 }
+
 /* ************************************************************************** */
 static int split_to_b_rec(t_data *d, int start, int end)
 {
@@ -92,27 +94,9 @@ static int split_to_b_rec(t_data *d, int start, int end)
 
 		}
 
-
-
-
-		// if (!is_same_chunk_id_whole_stack(&d->a))
-		// 	rev_rot_a_loop(d, cnt_rot_a);
-		// rev_rot_b_loop(d, cnt_rot_b);
-
-
-
-		// if (is_same_chunk_id_whole_stack(&d->a))
-		// 	cnt_rot_a = 0;
-		// rev_rot_a_loop(d, cnt_rot_a);
-		// rev_rot_b_loop(d, cnt_rot_b);
-
-
 		if (is_same_chunk_id_whole_stack(&d->a))
 			cnt_rot_a = 0;
 		optimize_rev_rot(d, cnt_rot_a, cnt_rot_b);
-
-
-
 		/* ------------------------------------------------------ */
 		split_to_b_rec(d, mid_2 + 1, end);
 	}
@@ -144,3 +128,30 @@ static void optimize_rev_rot(t_data *d, int cnt_rot_a, int cnt_rot_b)
 }
 
 /* ************************************************************************** */
+// static void optimize_rot(t_data *d, int *buf_rot_a, int *buf_rot_b)
+// {
+// 	int min_rot_2;
+// 	int diff;
+		
+// 	if (*buf_rot_a <= *buf_rot_b)
+// 	{
+// 		min_rot_2 = *buf_rot_a;
+// 		rotate_2_loop(d, min_rot_2);
+
+// 		diff = *buf_rot_b - *buf_rot_a;
+// 		rotate_b_loop(d, diff);
+// 	}
+// 	else
+// 	{
+// 		min_rot_2 = *buf_rot_b;
+// 		rotate_2_loop(d, min_rot_2);
+
+// 		diff = *buf_rot_a - *buf_rot_b;
+// 		rotate_a_loop(d, diff);
+// 	}
+
+// 	*buf_rot_a = 0;
+// 	*buf_rot_b = 0;
+// 	/* ------------------------------------------------------ */
+
+// }
