baaef0331b77cdc6964b9cf2b7d8e9fdfda6a77c
sorakann iMac <ski@student.42lausanne.com>
2022-03-15 12:31:53 +0100

    destruction de manage_parameter() pour la norminette

--- manage_parameter.c
+++ manage_parameter.c
@@ -12,6 +12,10 @@
 #include "push_swap.h"
 
 /* ************************************************************************** */
+static int	manage_is_param_ok(char **argv, int *i);
+static int	manage_is_duplicated(t_data *d, int *val, int *i);
+
+/* ************************************************************************** */
 bool	manage_parameter(int argc, char **argv, t_data *d)
 {
 	int		i;
@@ -23,30 +27,43 @@ bool	manage_parameter(int argc, char **argv, t_data *d)
 	i = 1;
 	while (i < argc)
 	{
-		/* --------------------------------------------- */
-		if (!is_param_ok(argv[i], i))
-		{
-			ft_printf("Error\n");
+		if (manage_is_param_ok(argv, &i) == EXIT_YES)
 			return (EXIT_YES);
-		}
-		/* --------------------------------------------- */
 		val = atoi(argv[i]);
-		if (is_duplicated(&d->a.head, val))
-		{
-			ps_del_list(&d->a.head);
-			ps_del_list(&d->b.head);
-			if (FLAG_INFO)
-				ft_printf("argv[%d]: is duplicated [%d]\n", i, val);
-			ft_printf("Error\n");
+		if (manage_is_duplicated(d, &val, &i) == EXIT_YES)
 			return (EXIT_YES);
-		}
-		/* --------------------------------------------- */
 		new = ps_new_elem(val);
 		ps_add_elem_to_bottom(new, &d->a);
 		(d->vpi)[i - 1].val = val;
 		d->vpi[i - 1].ptr = new;
 		d->vpi[i - 1].ptr->chk_id = argc - 2;
 		i++;
+	}
+	return (EXIT_NO);
+}
+
+/* ************************************************************************** */
+static int	manage_is_param_ok(char **argv, int *i)
+{	
+	if (!is_param_ok(argv[*i], *i))
+	{
+		ft_printf("Error\n");
+		return (EXIT_YES);
+	}
+	return (EXIT_NO);
+}
+
+/* ************************************************************************** */
+static int	manage_is_duplicated(t_data *d, int *val, int *i)
+{
+	if (is_duplicated(&d->a.head, *val))
+	{
+		ps_del_list(&d->a.head);
+		ps_del_list(&d->b.head);
+		if (FLAG_INFO)
+			ft_printf("argv[%d]: is duplicated [%d]\n", *i, *val);
+		ft_printf("Error\n");
+		return (EXIT_YES);
 	}
 	return (EXIT_NO);
 }
