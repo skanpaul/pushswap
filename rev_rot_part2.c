da3c0e2225778d75ec98b80454c62deeaa23ac6f
sorakann iMac <ski@student.42lausanne.com>
2022-03-15 12:02:39 +0100

    destruction du fichier rev_rot.c pour la norminette

--- rev_rot_part2.c
+++ rev_rot_part2.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rev_rot.c                                          :+:      :+:    :+:   */
+/*   rev_rot_part2.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ski <marvin@42lausanne.ch>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -10,56 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
-
-/* ************************************************************************** */
-/* PUSH the stack from the top to the bottom ******************************** */
-/* ************************************************************************** */
-void	rev_rot(t_stk *stk)
-{
-	t_ps	*extracted_elem;
-
-	if ((!stk) || (!has_2_elem_or_more(&stk->head)))
-		return ;
-	if (has_2_elem_only(&stk->head))
-		swap(stk);
-	else
-	{
-		extracted_elem = ps_rem_elem_from_bottom(stk);
-		ps_add_elem_to_top(extracted_elem, stk);
-	}
-	return ;
-}
-
-/* ************************************************************************** */
-void	rev_rot_a(t_data *d)
-{
-	rev_rot(&d->a);
-	ft_printf("rra\n");
-	d->cnt_cmd++;
-	display(d);
-	return ;
-}
-
-/* ************************************************************************** */
-void	rev_rot_b(t_data *d)
-{
-	rev_rot(&d->b);
-	ft_printf("rrb\n");
-	d->cnt_cmd++;
-	display(d);
-	return ;
-}
-
-/* ************************************************************************** */
-void	rev_rot_2(t_data *d)
-{
-	rev_rot(&d->a);
-	rev_rot(&d->b);
-	ft_printf("rrr\n");
-	d->cnt_cmd++;
-	display(d);
-	return ;
-}
 
 /* ************************************************************************** */
 void	rev_rot_a_loop(t_data *d, int loop)
