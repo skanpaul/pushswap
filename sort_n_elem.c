02d1f361e953fb615fac2b0ec1abae52d1bdb920
sorakann iMac <ski@student.42lausanne.com>
2022-03-17 18:21:56 +0100

    hard code pour les 5 combinaisons (sur 6) de liste de 3 chiffres

--- sort_n_elem.c
+++ sort_n_elem.c
@@ -20,7 +20,23 @@ void	sort_2_elem(t_data *d)
 /* ************************************************************************** */
 void	sort_3_elem(t_data *d)
 {
-	if ((d->a.head->index == 2) && (d->a.head->next->index == 1)
+	if ((d->a.head->index == 1) && (d->a.head->next->index == 2)
+		&& (d->a.head->next->next->index == 0))
+		rev_rot_a(d);
+	else if ((d->a.head->index == 2) && (d->a.head->next->index == 0)
+		&& (d->a.head->next->next->index == 1))
+	{
+		swap_a(d);
+		rev_rot_a(d);
+		swap_a(d);
+	}
+	else if ((d->a.head->index == 0) && (d->a.head->next->index == 2)
+		&& (d->a.head->next->next->index == 1))
+	{
+		rev_rot_a(d);
+		swap_a(d);
+	}
+	else if ((d->a.head->index == 2) && (d->a.head->next->index == 1)
 		&& (d->a.head->next->next->index == 0))
 	{
 		swap_a(d);
@@ -29,13 +45,17 @@ void	sort_3_elem(t_data *d)
 	else if ((d->a.head->index == 1) && (d->a.head->next->index == 0)
 		&& (d->a.head->next->next->index == 2))
 		swap_a(d);
-	else
-	{
-		if (d->a.head->index > d->a.head->next->index)
-			swap_a(d);
-		rotate_a(d);
-		if (d->a.head->index > d->a.head->next->index)
-			swap_a(d);
-		rev_rot_a(d);
-	}
 }
+
+/* ************************************************************************** */
+//"0 1 2"
+//"1 2 0"
+//"2 0 1"
+
+//"0 2 1"
+//"2 1 0"
+//"1 0 2"
+
+//"1 0 2" X
+//"0 2 1" X
+//"2 1 0" X
